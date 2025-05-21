10d9cafb48a0363978622cc29a0301a39543dcd7
MouizLMNy <120992513+MouizLMNy@users.noreply.github.com>
2025-05-21 10:47:43 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from cryptography.fernet import Fernet
-from flask import Flask, render_template_string, render_template, jsonify
+from flask import Flask, render_template_string, render_template, jsonify, request
 from flask import render_template
 from flask import json
 from urllib.request import urlopen
